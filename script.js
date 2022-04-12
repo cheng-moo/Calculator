6d897961c4925bf19d6185071ca3cdeb29cd4670
Cheng_Mo_ <Cheng_Mo_@gmail.com>
2022-04-12 21:57:09 +0200

    Add Listeners to btns

---
+++ script.js
@@ -0,0 +1,30 @@
+//Select Calculator to add listeners to btns
+const calculator = document.querySelector('.calculator');
+const keys = calculator.querySelector('.calculator__keys');
+
+keys.addEventListener('click', e => {
+    if (e.target.matches('button')) {
+        const key = e.target;
+        const action = key.dataset.action;
+        if(!action) {
+            console.log('number key!');
+        }
+        if (
+            action === 'add' ||
+            action === 'subtract' ||
+            action === 'multiply' ||
+            action === 'divide'
+        ) {
+            console.log('operator key!');
+        }
+       if (action === 'decimal') {
+           console.log('decimal key!');
+       } 
+       if (action === 'clear') {
+           console.log('clear key!');
+       }
+       if (action === 'calculate') {
+           console.log('equal key!');
+       }
+    }
+})
