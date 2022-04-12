02fdb261e2a5598e909b142b633f2fa283ef8d69
Cheng_Mo_ <Cheng_Mo_@gmail.com>
2022-04-12 02:19:00 +0200

    Add Operator and Operations Functions

---
+++ script.js
@@ -0,0 +1,27 @@
+
+//Operation Functions
+function add (n1, n2) {
+    return n1 + n2;
+}
+function subtract (n1, n2) {
+    return n1 - n2;
+}
+function multiply (n1, n2) {
+    return n1 * n2;
+}
+function divide (n1, n2) {
+    return n1 / n2;
+}
+//Operator Function 
+
+function operator (operator, n1, n2) {
+    if (operator === '+') {
+        add(n1, n2);
+    } else if (operator === '-') {
+        subtract(n1, n2);
+    } else if (operator === '*') {
+        multiply(n1, n2);
+    } else if (operator === '/') {
+        divide(n1, n2);
+    }
+}
