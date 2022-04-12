b9db0397c414d30013a794d5228afb6be9941197
Cheng_Mo_ <Cheng_Mo_@gmail.com>
2022-04-12 02:37:13 +0200

    Display Numbers

--- script.js
+++ script.js
@@ -25,3 +25,9 @@ function operator (operator, n1, n2) {
         divide(n1, n2);
     }
 }
+const display = document.querySelector('.display');
+const numBtns = document.querySelectorAll('.num');
+numBtns.forEach(btn => btn.addEventListener('click', displayVal))
+function displayVal (e) {
+    display.textContent = e.target.textContent;
+}
