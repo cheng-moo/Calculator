49d24ae8e64a9a348bfcbe4b7b8f5c97414b68f4
Cheng_Mo_ <Cheng_Mo_@gmail.com>
2022-04-13 23:53:30 +0200

    Clean up

--- script.js
+++ script.js
@@ -14,18 +14,18 @@ keys.addEventListener('click', e => {
             action === 'subtract' ||
             action === 'multiply' ||
             action === 'divide'
-        ) {
-            console.log('operator key!');
-        }
-       if (action === 'decimal') {
-           console.log('decimal key!');
-       } 
+            ) {
+                console.log('operator key!');
+            }
+            if (action === 'decimal') {
+                console.log('decimal key!');
+            } 
        if (action === 'clear') {
            console.log('clear key!');
-       }
-       if (action === 'calculate') {
-           console.log('equal key!');
-       }
+        }
+        if (action === 'calculate') {
+            console.log('equal key!');
+        }
     }
 })
 //When the user hits a number key
@@ -33,16 +33,22 @@ keys.addEventListener('click', e => {
 // And the current displayed number
 const display = document.querySelector('.calculator__display')
 
+//The main function
 keys.addEventListener('click', e => {
-    if (e.target.matches('button')) {
-        const key = e.target;
-        const action = key.dataset.action;
-        const keyContent = key.textContent;
-        const displayedNum = display.textContent;
-        console.log(key, action, keyContent, displayedNum);
-        const previousKeyType = calculator.dataset.previousKeyType;
-        
-        const createResultString = (key, displayedNum, state) => {
+    if (!e.target.matches('button')) return;
+    const key = e.target;
+    const displayedNum = display.textContent;
+    //Pure functions
+    const resultString = createResultString(key, displayedNum, calculator.dataset)
+
+    //Update states
+    display.textContent = resultString;
+    updateCalculatorState(key, calculator, resultString, displayedNum);
+    updateVisualState(key, calculator);
+    }
+)
+
+       const createResultString = (key, displayedNum, state) => {
             //Variables required are:
             //1. keyContent
             //2. displayedNum
@@ -52,11 +58,13 @@ keys.addEventListener('click', e => {
             //6. calculator.dataset.operator
             //7. calculator.dataset.modValue
             const keyContent = key.textContent;
-            const action = key.dataset.action;
-            const firstValue = state.firstValue;
-            const modValue = state.modValue;
-            const operator = state.operator;
-            const previousKeyType = state.previousKeyType;
+           
+            const { 
+                 firstValue,
+                 modValue,
+                 operator,
+                 previousKeyType,
+            } = state;
             //... Refactor as necessary
             const keyType = getKeyType(key)
             
@@ -66,9 +74,7 @@ keys.addEventListener('click', e => {
                 previousKeyType === 'calculate'
                 ? keyContent
                 : displayedNum + keyContent;
-                
-                calculator.dataset.previousKeyType = 'number';
-            }
+                }
             //When the user hits the decimal key
             if (keyType === 'decimal') {
                 if(!displayedNum.includes('.')) return displayedNum + '.'
@@ -77,14 +83,9 @@ keys.addEventListener('click', e => {
                     previousKeyType === 'calculate'
                     ) return '0.';
                     return displayedNum;
-                    
-                    calculator.dataset.previousKeyType = 'decimal';
                 }
                 //When the user hits an operator key
-                if (keyType === 'operator') {
-                    const firstValue = calculator.dataset.firstValue;
-                    const operator = calculator.dataset.operator;
-                    
+                if (keyType === 'operator') { 
                     return firstValue &&
                     operator &&
                     previousKeyType !== 'operator' &&
@@ -93,11 +94,7 @@ keys.addEventListener('click', e => {
                     : displayedNum;
                 }
                 if (keyType === 'clear') return 0;
-                if (keyType === 'calculate') {
-                    let firstValue = calculator.dataset.firstValue;
-                    const operator = calculator.dataset.operator;
-                    let modValue = calculator.dataset.modValue;
-                    
+                if (keyType === 'calculate') {  
                     return firstValue
                     ? previousKeyType === 'calculate'
                     ? calculate(displayedNum, operator, modValue)
@@ -107,19 +104,6 @@ keys.addEventListener('click', e => {
                 }
             }
             
-            //Using createResultString function
-            keys.addEventListener('click', e => {
-                if (e.target.matches('button')) return
-                const key = e.target;
-                const displayedNum = display.textContent;
-                //Pure functions
-                const resultString = createResultString(key, displayedNum, calculator.dataset)
-
-                //Update states
-                display.textContent = resultString;
-                updateCalculatorState(key, calculator, resultString, displayedNum);
-                updateVisualState(key, calculator);
-            })
             
             //getKeyType function
             const getKeyType = (key) => {
@@ -144,16 +128,17 @@ keys.addEventListener('click', e => {
                     //5. modValue
 
                     const keyType = getKeyType(key);
+                    const {
+                        firstValue,
+                        operator, 
+                        modValue,
+                        previousKeyType,
+                    } = calculator.dataset;
+
                     calculator.dataset.previousKeyType = keyType;
-                    //When the user hits a number key after an operator key
-                    //remove .is-depressed class from all keys
-                    Array.from(key.parentNode.children)
-                    .forEach(k => k.classList.remove('is-depressed'));
+                  
                     
-                    if (keyType === 'number') {}
-                    if (keyType === 'decimal') {}
                     if (keyType === 'operator') {
-                        key.classList.add('is-depressed')
                         calculator.dataset.operator = key.dataset.action;
                         calculator.dataset.firstValue = firstValue &&
                              operator &&
@@ -163,24 +148,20 @@ keys.addEventListener('click', e => {
                           : displayedNum;
                           
                     }
-                    if (keyType === 'clear') {
+                    if (keyType === 'clear' && key.textContent === 'AC') {
                              //clear has two uses
                              //AC to clear all and resets initial state (default)
                              //CE clears current entry and keeps prev numbers in memory
-                        if (key.textContent === 'AC') {
+                    
                         calculator.dataset.firstValue = '';
                         calculator.dataset.modValue = '';
                         calculator.dataset.operator = '';
                         calculator.dataset.previousKeyType = '';
-                        } else {
-                        key.textContent = 'AC';
+                       
                        }
-                    }
-                    if (keyType !== 'clear') {
-                        const clearButton = calculator.querySelector('[data-action = clear]');
-                        clearButton.textContent = 'CE';
+                    
 
-                    }
+                    
                     if (keyType === 'calculate') {
                         calculator.dataset.modValue = firstValue && previousKeyType === 'calculate'
                         ? modValue
@@ -202,61 +183,7 @@ keys.addEventListener('click', e => {
                         clearButton.textContent = 'CE';
                     }
                 }
-                if (
-                    action === 'add' ||
-                    action === 'subtract' ||
-                    action === 'multiply' ||
-                    action === 'divide'
-                    ) {
-                        key.classList.add('is-depressed');
-                        //Add custom attribute
-                calculator.dataset.previousKeyType = 'operator';
-                calculator.dataset.firstValue = displayedNum;
-                calculator.dataset.operator = action;
-            }
-            if (action !== 'clear') {
-                const clearButton = calculator.querySelector('[data-action = clear]');
-                clearButton.textContent = 'CE';
-            }
-            if (action === 'clear') {
-                //clear has two uses
-                //AC to clear all and resets initial state (default)
-                //CE clears current entry and keeps prev numbers in memory
-                if (key.textContent === 'AC') {
-                    calculator.dataset.firstValue = '';
-                    calculator.dataset.modValue = '';
-                    calculator.dataset.operator = '';
-                    calculator.dataset.previousKeyType = '';
-                } else {
-                    key.textContent = 'AC';
-                }
-                calculator.dataset.previousKeyType = 'clear'
-            }
-            //When the user hits the equals key
-            //info needed: first num, operator, second num
-            if (action === 'calculate') {
-                if (
-                    firstValue &&
-                     operator &&
-                     previousKeyType !== 'operator' &&
-                     previousKeyType !== 'calculate'
-                  ) {
-                    const calcValue = calculate(firstValue, operator, secondValue);
-                    display.textContent = calcValue;
-
-                    //Update calculated value as firstValue
-                    calculator.dataset.firstValue = calcValue;
-                } else {
-                    //if there are no calculations, set displayedNum as the firstValue
-                    calculator.dataset.firstValue = displayedNum;
-                }
-                //set modValue attribute
-                calculator.dataset.modValue = secondValue;
-                calculator.dataset.previousKeyType = 'calculate'
-            }
-        }
-    })
-    
+              
     const calculate = (n1, operator, n2) => {
         const firstNum = parseFloat(n1);
         const secondNum = parseFloat(n2);
